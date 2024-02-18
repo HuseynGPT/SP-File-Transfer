cb65f002a9bd0fe94a82563e3df4ca4b6720cd76
HuseynGPT <huseynovbabek1@gmail.com>
2024-02-18 17:50:32 +0400

    Add project files.

---
+++ File Transfer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using File_Transfer.Services;
+using System.Windows.Input;
+
+namespace File_Transfer.ViewModels;
+
+public class MainWindowViewModel:NotificationService
+{
+    private string? fromPath;
+
+    public string FromPath { get => fromPath!; set { fromPath = value;OnPropertyChanged(); } }
+
+    private string? toPath;
+
+    public string ToPath { get => toPath!; set { toPath = value; OnPropertyChanged(); } }
+
+
+    public ICommand FromBtnCommand { get; set; }
+    public ICommand ToBtnCommand { get; set; }
+
+
+
+
+
+}
